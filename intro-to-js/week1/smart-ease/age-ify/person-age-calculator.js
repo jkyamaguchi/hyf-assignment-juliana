f81f8743be9f5445923d9e6345981832a552ba79
Juliana Yamaguchi <ju.yamaguchi@gmail.com>
2025-11-07 15:21:08 +0100

    Update intro-to-js/week1/smart-ease/age-ify/person-age-calculator.js
    Change variables declaration to const as suggested by Jesús.
    
    Co-authored-by: Jesús Otero <jesusoterogomez+sub@gmail.com>

--- intro-to-js/week1/smart-ease/age-ify/person-age-calculator.js
+++ intro-to-js/week1/smart-ease/age-ify/person-age-calculator.js
@@ -6,9 +6,9 @@ function calculateAge(yearOfBirth, yearFuture) {
   return yearFuture - yearOfBirth;
 }
 
-yearOfBirth = 1983;
-yearFuture = 2050;
+const yearOfBirth = 1983;
+const yearFuture = 2050;
 
-age = calculateAge(yearOfBirth, yearFuture);
+const age = calculateAge(yearOfBirth, yearFuture);
 
 console.log(`You will be ${age} years old in ${yearFuture}.`);
