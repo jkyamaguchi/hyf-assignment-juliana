c193b9274977b181a9651ff01b0373e04eba9e44
Juliana <ju.yamaguchi@gmail.com>
2026-03-14 22:48:35 +0100

    fix: only teas with stockCount < 50

--- advanced-javascript/week1/assignment/exercise3.js
+++ advanced-javascript/week1/assignment/exercise3.js
@@ -2,8 +2,9 @@ import { teas } from "../../data/teas.js";
 
 function lowStockAlert(teas) {
   // Return array of objects with name and stockCount
-  // sorted by stockCount (lowest first)
+  // Only teas with stockCount lower than 50, sorted by stockCount (lowest first)
   const result = teas
+    .filter((tea) => tea.stockCount < 50)
     .map((tea) => ({ name: tea.name, stockCount: tea.stockCount }))
     .sort((a, b) => a.stockCount - b.stockCount);
   return result;
