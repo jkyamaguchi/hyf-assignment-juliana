7bfbbaf90ffccc02736d5f32cd0b8051c1de1c47
Juliana <ju.yamaguchi@gmail.com>
2026-04-04 01:26:25 +0200

    assignment: exercise 1 - Tea class with validation

---
+++ advanced-javascript/week4/assignment/exercise1.js
@@ -0,0 +1,25 @@
+import { teas } from "../../data/teas.js";
+import { Tea } from "../Tea.js";
+
+// Build a complete Tea class with validation and a static factory method.
+
+// Test validation:
+try {
+  new Tea("", "green", "Japan", 0.12, true);
+} catch (e) {
+  console.log(e.message);
+} // "Name is required"
+
+try {
+  new Tea("Test", "purple", "Japan", 0.12, true);
+} catch (e) {
+  console.log(e.message);
+} // "Invalid type: purple"
+
+// Test factory method:
+const teaInstances = teas.map(Tea.fromObject);
+console.log(teaInstances.length); // 20
+console.log(teaInstances[0].describe());
+// "Sencha (green) from Japan - 12.00 DKK/100g [organic]"
+console.log(teaInstances[1].describe());
+// "Earl Grey (black) from India - 8.00 DKK/100g"
