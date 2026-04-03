0896ec10379f6df770e40d78c0bb0fdffc33d65c
Juliana <ju.yamaguchi@gmail.com>
2026-04-04 00:27:56 +0200

    in-class: OrderItem class

---
+++ advanced-javascript/week4/OrderItem.js
@@ -0,0 +1,25 @@
+import { Tea } from "./Tea.js";
+
+class OrderItem {
+  constructor(tea, grams) {
+    if (!(tea instanceof Tea)) {
+      throw new Error("tea must be a Tea instance");
+    }
+    if (typeof grams !== "number" || grams <= 0) {
+      throw new Error("grams must be a positive number");
+    }
+    this.tea = tea;
+    this.grams = grams;
+  }
+
+  lineTotal() {
+    // use the tea's priceFor method
+    return this.tea.priceFor(this.grams);
+  }
+
+  describe() {
+    return `${this.grams}g ${this.tea.name} - ${this.lineTotal().toFixed(2)} DKK`;
+  }
+}
+
+export { OrderItem };
